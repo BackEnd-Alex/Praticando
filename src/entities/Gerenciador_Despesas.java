1e487fb388f785de4a866392ab2f849bf5d8bb86
Alexsandro Mendes <contadetestealexsandro@gmail.com>
2024-10-23 16:56:09 -0300

    "Gerando uma lista de despesas."

---
+++ src/entities/Gerenciador_Despesas.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gerenciador_Despesas {
+
+    private List<Expense> despesas;
+
+    public Gerenciador_Despesas() {
+        this.despesas = new ArrayList<>();
+    }
+
+}
